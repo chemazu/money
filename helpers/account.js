358efdde27da73c8fb17501b2122d6efbbd7bda6
unknown <chemazu>
2021-05-08 09:51:31 +0100

    reuseable functions v1 done

--- helpers/account.js
+++ helpers/account.js
@@ -1,19 +1,26 @@
 const fetch = require('node-fetch');
 
-let milk = fun()
-module.exports={
-  getAccountId :async(req,res,next)=>{
-    const url = 'https://api.withmono.com/account/auth';
+async function handleAccountId(code){
+  const url = 'https://api.withmono.com/account/auth';
     const options = {
         method: 'POST',
         headers: {'mono-sec-key': 'live_sk_uauQJ2QMGBTeMmIzDVAU', 'Content-Type': 'application/json'},
-        body: JSON.stringify({code: `${req.body.code}`})
-      };
-      fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
-    res.send("went through")
+        body: JSON.stringify({code: `${code}`})
+      }
+      const response =await fetch  (url,options)
+      return response.json()
+  // fetch(url, options)
+  // .then(res => res.json())
+  // .then(json => {return json})
+  // .catch(err => console.error('error:' + err));
+}
+
+module.exports={
+  getAccountId :async(req,res,next)=>{
+   const{ code} = req.body
+    const accountId = await handleAccountId(code)
+    console.log(accountId)
+    res.status(200).json(accountId)
     next()
 
   },
